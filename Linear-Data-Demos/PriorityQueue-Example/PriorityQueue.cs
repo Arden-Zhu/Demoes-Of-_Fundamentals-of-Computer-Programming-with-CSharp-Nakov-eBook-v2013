f758a2c554ecd62f13f169a9f1da64e67521e6bd
Arden Zhu <arden.zhu@lafayette148.com>
2019-06-09 19:47:57 -0400

    init

---
+++ Linear-Data-Demos/PriorityQueue-Example/PriorityQueue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Wintellect.PowerCollections;
+
+namespace PriorityQueue_Example
+{
+    class PriorityQueue<T> where T : IComparable<T>
+    {
+        private OrderedBag<T> bag;
+
+        public int Count
+        {
+            get
+            {
+                return bag.Count;
+            }
+            private set
+            {
+            }
+        }
+
+        public PriorityQueue()
+        {
+            bag = new OrderedBag<T>();
+        }
+
+        public void Enqueue(T element)
+        {
+            bag.Add(element);
+        }
+
+        public T Dequeue()
+        {
+            var element = bag.GetFirst();
+            bag.RemoveFirst();
+            return element;
+        }
+    }
+}
---
+++ Linear-Data-Demos/PriorityQueue-Example/PriorityQueueExample.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace PriorityQueue_Example
+{
+    class PriorityQueueExample
+    {
+        static void Main()
+        {
+            PriorityQueue<Person> people = new PriorityQueue<Person>();
+            people.Enqueue(new Person("Pesho", 11));
+            people.Enqueue(new Person("malkiq Goshko", 9));
+            people.Enqueue(new Person("Mimeto", 21));
+            people.Enqueue(new Person("Baba Mara", 70));
+            people.Enqueue(new Person("Bai Ivan", 65));
+            people.Enqueue(new Person("Bobi", 15));
+            people.Enqueue(new Person("Pena", 45));
+            people.Enqueue(new Person("Krasito", 50));
+            while (people.Count > 0)
+            {
+                Console.WriteLine(people.Dequeue());
+            }
+        }
+    }
+
+    class Person :IComparable<Person>
+    {
+        string Name { get; set; }
+
+        int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}, {1}", Name, Age);
+        }
+        public int CompareTo(Person other)
+        {
+            return this.Age.CompareTo(other.Age);
+        }
+    }
+}
